492adc18b37df684b056227ebe09a0b9fbed8193
a90180344 <940180344@qq.com>
2022-08-12 20:12:13 +0800

    11

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -35,7 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:20111`,
+        target: `http://112.74.44.130:201`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
